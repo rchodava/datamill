018524eae0bd568feab6103c4a2920eda5cc627b
rchodava <rchodava@gmail.com>
2015-10-11 22:34:48 +0100

    - Fix request data handling

--- core/src/main/java/org/chodavarapu/datamill/http/impl/RequestEntityImpl.java
+++ core/src/main/java/org/chodavarapu/datamill/http/impl/RequestEntityImpl.java
@@ -1,13 +1,11 @@
 package org.chodavarapu.datamill.http.impl;
 
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.rx.java.RxHelper;
 import org.chodavarapu.datamill.http.RequestEntity;
 import org.chodavarapu.datamill.http.HttpException;
 import org.chodavarapu.datamill.json.JsonObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import rx.Observable;
-import rx.observables.AbstractOnSubscribe;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,8 +14,6 @@ import java.io.IOException;
  * @author Ravi Chodavarapu (rchodava@gmail.com)
  */
 public class RequestEntityImpl implements RequestEntity {
-    private static final Logger logger = LoggerFactory.getLogger(RequestEntityImpl.class);
-
     private final HttpServerRequest request;
 
     public RequestEntityImpl(HttpServerRequest request) {
@@ -25,8 +21,8 @@ public class RequestEntityImpl implements RequestEntity {
     }
 
     @Override
-    public byte[] asBytes() {
-        return asChunks().<ByteArrayOutputStream>collect(
+    public Observable<byte[]> asBytes() {
+        return asChunks().collect(
                 () -> new ByteArrayOutputStream(),
                 (stream, chunk) -> {
                     try {
@@ -34,36 +30,21 @@ public class RequestEntityImpl implements RequestEntity {
                     } catch (IOException e) {
                         throw new HttpException(e);
                     }
-                }).toBlocking().last().toByteArray();
+                }).map(os -> os.toByteArray());
     }
 
     @Override
     public Observable<byte[]> asChunks() {
-        return AbstractOnSubscribe.<byte[], Void>create(
-                s -> {
-                    request.handler(buffer -> s.onNext(buffer.getBytes()));
-                    request.exceptionHandler(exception -> s.onError(exception));
-                    request.endHandler(v -> s.onCompleted());
-                },
-                s -> null,
-                v -> {
-                    try {
-                        request.handler(null);
-                        request.exceptionHandler(null);
-                        request.endHandler(null);
-                    } catch (IllegalStateException e) {
-                        logger.error("Error on terminating request data handling!", e);
-                    }
-                }).toObservable();
+        return RxHelper.toObservable(request).map(b -> b.getBytes());
     }
 
     @Override
-    public JsonObject asJson() {
-        return new JsonObject(asString());
+    public Observable<JsonObject> asJson() {
+        return asString().map(string -> new JsonObject(string));
     }
 
     @Override
-    public String asString() {
-        return new String(asBytes());
+    public Observable<String> asString() {
+        return asBytes().map(bytes -> new String(bytes));
     }
 }
