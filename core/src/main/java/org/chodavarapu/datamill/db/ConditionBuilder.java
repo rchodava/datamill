7c106b89e2bc6ec1b8626aeed998ec507aa04d40
israel <israel@solidstategroup.com>
2016-02-20 14:27:16 +0000

    Add the possibility to chain conditions with 'AND' clause. Add 'IS' clause.

--- core/src/main/java/org/chodavarapu/datamill/db/ConditionBuilder.java
+++ core/src/main/java/org/chodavarapu/datamill/db/ConditionBuilder.java
@@ -6,7 +6,11 @@ import org.chodavarapu.datamill.reflection.Member;
  * @author Ravi Chodavarapu (rchodava@gmail.com)
  */
 public interface ConditionBuilder<R> {
-    <T> R eq(String column, T value);
-    <T> R eq(String table, String column, T value);
-    <T> R eq(Member member, T value);
+    <T> WhereBuilder<R> eq(String column, T value);
+    <T> WhereBuilder<R> eq(String table, String column, T value);
+    <T> WhereBuilder<R> eq(Member member, T value);
+
+    <T> WhereBuilder<R> is(String column, T value);
+    <T> WhereBuilder<R> is(String table, String column, T value);
+    <T> WhereBuilder<R> is(Member member, T value);
 }
