47e5927a6b195dd5dd81cdf7c747b6e8747ddac6
Ravi Chodavarapu <rchodava@gmail.com>
2016-03-02 12:38:51 +0000

    Add error responses to ResponseBuilder

--- core/src/main/java/org/chodavarapu/datamill/http/ResponseBuilder.java
+++ core/src/main/java/org/chodavarapu/datamill/http/ResponseBuilder.java
@@ -6,10 +6,15 @@ import rx.Observable;
  * @author Ravi Chodavarapu (rchodava@gmail.com)
  */
 public interface ResponseBuilder {
+    Observable<Response> badRequest();
+    Observable<Response> badRequest(String content);
     <T> ResponseBuilder header(String name, T value);
+    Observable<Response> internalServerError();
+    Observable<Response> internalServerError(String content);
     Observable<Response> noContent();
     Observable<Response> notFound();
     Observable<Response> ok();
     Observable<Response> ok(String content);
     Observable<Response> unauthorized();
+    Observable<Response> unauthorized(String content);
 }
