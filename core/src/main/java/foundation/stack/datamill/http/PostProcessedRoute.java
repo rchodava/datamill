0e402d5192962f91f78bb030ddbfa0bb1e43fa76
Tiago Roldão <focus5.6@gmail.com>
2016-10-13 18:24:07 +0100

    Pass request to postProcessor function calls

--- core/src/main/java/foundation/stack/datamill/http/PostProcessedRoute.java
+++ core/src/main/java/foundation/stack/datamill/http/PostProcessedRoute.java
@@ -1,10 +1,10 @@
 package foundation.stack.datamill.http;
 
-import rx.functions.Func1;
+import rx.functions.Func2;
 
 /**
  * @author Ravi Chodavarapu (rchodava@gmail.com)
  */
 public interface PostProcessedRoute extends Route {
-    Route andFinally(Func1<Response, Response> postProcessor);
+    Route andFinally(Func2<Request, Response, Response> postProcessor);
 }
